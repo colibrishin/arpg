8eecc03bb64f0abbd25a6298177a6a3a9ce099a3
Colibri Shin <colibrishin@gmail.com>
2025-06-12 00:02:30 +0900

    feat: native ammo widget class

---
+++ Source/ARPG/Public/Weapon/Widget/NAAmmoWidget.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NAAmmoWidget.generated.h"
+
+class UImage;
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class ARPG_API UNAAmmoWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ammo", meta=(BindWidget, AllowPrivateAccess="true"))
+	UImage* Background;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Ammo", meta=(BindWidget, AllowPrivateAccess="true"))
+	UTextBlock* AmmoCount;
+};
